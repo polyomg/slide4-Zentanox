6182d19925502d13d5c4d1a0bb809261d9a43c3a
Zentanox <goddeadpokemon@gmail.com>
2025-10-02 13:34:13 +0700

    TS00705-NguyenMinhNhat-Slide4-First commit

---
+++ lab4/src/test/java/web/shop/lab4/Lab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package web.shop.lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
